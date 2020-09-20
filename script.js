33aef329c28e342f8c38b467ae841497ef63ce3e
prashantpawale25077 <70219906+prashantpawale25077@users.noreply.github.com>
2020-09-20 11:18:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//ARRAY
+var array = new Array();
+array[0] = " Yaakov";
+array[1] = " John";
+array[2] = " jen" ;
+array[3] =" jason";
+array[4] = " Paul";
+array[5] = " Frank";
+array[6] = " Larry" ;
+array[7] =" Paula";
+array[8] = " Laura";
+array[9] = " Jim";
+array[10] = function (name) { 
+	document.write(" hello" + name );
+
+};
+array[11] = function (name1) {
+	document.write(" Good Bye" + name1 );
+};
+array[10] (array[0]);
+array[11] (array[1]);
+array[11] (array[2]);
+array[11] (array[3]);
+array[10] (array[4]);
+array[10] (array[5]);
+array[10] (array[6]);
+array[10] (array[7]);
+array[10] (array[8]);
+array[11] (array[9]);
+
+
+
